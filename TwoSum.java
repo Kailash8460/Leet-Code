7762c1acc98153db1279e0c69d65b502fc943216
Kailash8460 <kailashbhagchandani60@gmail.com>
2026-01-19 01:58:05 +0530

    Solution with the Time Complexity with the Actual Solution code

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,8 @@
+// 1. Two Sum
+
 import java.util.*;
 
+// Not Within Time Complexity - for Begineers to understand the ArrayList and Loop concept
 public class TwoSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -52,5 +55,36 @@ public class TwoSum {
             }
         }
         return result;
+    }
+}
+
+// Within Time Complexity - Adavnced Solution
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+}
+
+// Actual Solution
+class ActualSolution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // If no valid pair is found, return an empty array instead of null
+        return new int[] {};
     }
 }
