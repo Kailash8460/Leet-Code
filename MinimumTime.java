ecdc5e6a600b850abee9a2407c519c984cfa9514
Kailash8460 <kailashbhagchandani60@gmail.com>
2026-01-19 01:00:04 +0530

    All Leet Code Solutions Here.

---
+++ MinimumTime.java
@@ -0,0 +1,17 @@
+// 1266. Minimum Time Visiting All Points
+
+public class MinimumTime {
+    public static void main(String[] args) {
+
+    }
+
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        for (int i = 1; i < points.length; i++) {
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
+            time += Math.max(dx, dy);
+        }
+        return time;
+    }
+}
